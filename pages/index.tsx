ca8f40a937258164fc655e86e6c3000510291fed
Jaywanth J <jaywanth.gowda11@gmail.com>
2022-11-13 16:00:33 +0530

    feat: Add dynamic routing to project and populate values

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,24 @@ import { HeaderMegaMenu } from '../components/Header/Header';
 import Dashboard from './dashboard';
 import Project from './project';
 
-export default function HomePage() {
+export default function HomePage({projects}) {
   return (
     <>
       <HeaderMegaMenu/>
-      <Project />
+      <Dashboard props={{projects}}/>
     </>
   );
   }
 
+  export async function getStaticProps() {
+    const res = await fetch('http://194.195.119.85:8000/projects', {
+      method: 'GET',
+    });
+    const projects = await res.json();
+  
+    return {
+      props: {
+        projects,
+      },
+    };
+  }
---
+++ pages/project/[id].tsx
@@ -0,0 +1,72 @@
+import { useRef, useState, useCallback, useMemo } from "react";
+import { Container } from '@mantine/core';
+import { Space } from '@mantine/core';
+import { Grid } from '@mantine/core';
+import { Box } from '@mantine/core';
+import { Title } from '@mantine/core';
+import { Button } from '@mantine/core';
+import { Text } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { openConfirmModal } from '@mantine/modals';
+import { Section, sectionProps } from '../../components/Section/SectionComponent';
+import { Modal, Group } from '@mantine/core';
+import * as React from 'react';
+
+function Project({projects}){
+  const sections = ['Data Cleaning', 'Feature Engineering', 'Model', 'Metrics']
+  const [projectName, setProjectName] = useState(['Project Name']); 
+
+  console.log(projects);
+  return(
+        <Container size="md">
+
+            <Title order={2}>
+                {projects?.name==null?projectName:projects.name}
+            </Title>
+
+            <Space h='lg'></Space>
+
+            <Grid>
+
+                {sections.map((item) => (
+                    <Grid.Col span={6}>
+                        <Section sectionName={item} />
+                    </Grid.Col>
+                ))}    
+                    
+            </Grid>
+
+            <Space h='lg'></Space>
+
+            <Group position="right">
+
+                <Button>
+                    Download
+                </Button>
+
+            </Group>
+
+        </Container>
+
+  )
+
+}
+
+export default Project;
+
+export async function getServerSideProps(context) {
+  
+  // const router = useRouter()
+  const { id } = context.query
+  
+  const res = await fetch('http://194.195.119.85:8000/project/'+id, {
+    method: 'GET',
+  });
+  const projects = await res.json();
+
+  return {
+    props: {
+      projects,
+    },
+  };
+}
